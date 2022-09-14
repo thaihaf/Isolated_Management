3b9bf10a2fc6f6f43f6c52bdfe4378ee1476c17e
halh160328 <halhhe160328@fpt.edu.vn>
2022-09-14 22:16:31 +0700

    commit code again

--- src/java/dao/DBContext.java
+++ src/java/dao/DBContext.java
@@ -27,14 +27,14 @@ public abstract class DBContext<T>{
 
     protected Connection connection;
     
-    public DBContext(){
+     public DBContext() {
         try {
-            String user = "sa";
-            String pass = "123456";
-            String url = "jdbc:sqlserver://DESKTOP-IOV8322\\SQLEXPRESS:1433;databaseName=SWP_Project";
+            String user = "halh";
+            String pass = "1";
+            String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=SWP_Project";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, user, pass);
-        } catch (ClassNotFoundException | SQLException ex){
+        } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
         }
     }

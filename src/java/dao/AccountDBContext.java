e1b031f9bc656213ea3329067aaf17efd54b4070
Nguyễn Tuấn Dũng <dungnthe150915@fpt.edu.vn>
2022-09-14 17:51:34 +0700

    dungnt
    rename file "model" to "entity"

--- src/java/dao/AccountDBContext.java
+++ src/java/dao/AccountDBContext.java
@@ -10,7 +10,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import model.Account;
+import entity.Account;
 
 /**
  *

31fa3ba3cbef1ed7b912cda6d306f2be96500b38
ThanhntTbQl <106000334+ThanhntTbQl@users.noreply.github.com>
2022-09-17 01:32:16 +0700

    add user management

---
+++ src/java/entity/Role.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+public class Role {
+
+    private int id;
+    private String role;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+}

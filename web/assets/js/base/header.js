c8d31640f194bd3886ce973d99c489892d88357b
thaihaf <hanthe140188@fpt.edu.vn>
2022-09-17 09:12:44 +0700

    add Js to show and hide Sidebar

--- web/assets/js/base/header.js
+++ web/assets/js/base/header.js
@@ -2,13 +2,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
  */
-$(document).ready();
+
+let body = $("body");
+let menu = $(".menu");
 
 $(".user_info").click(() => {
-    let menu = $(".menu");
-    if ($(".menu").hasClass("hidden")) {
+    if (menu.hasClass("hidden")) {
         menu.removeClass("hidden");
     } else {
         menu.addClass("hidden");
+    }
+
+});
+
+$("#header_btnMenu").click(() => {
+    if (body.hasClass("hideSidebar")) {
+        body.removeClass("hideSidebar");
+    } else {
+        body.addClass("hideSidebar");
     }
 });

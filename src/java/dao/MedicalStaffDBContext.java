b11ed351c4a64a778ff52332af27fee658d036bb
ThanhntTbQl <106000334+ThanhntTbQl@users.noreply.github.com>
2022-09-18 09:02:21 +0700

    add user management complete
    thanhnthe160757

---
+++ src/java/dao/MedicalStaffDBContext.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import entity.AccountDetail;
+import entity.MedicalStaff;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Mountain
+ */
+public class MedicalStaffDBContext extends DBContext<MedicalStaff> {
+
+    @Override
+    public ArrayList<MedicalStaff> list() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public MedicalStaff get(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void insert(MedicalStaff model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void update(MedicalStaff model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void delete(MedicalStaff model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public MedicalStaff getByAccountDetail(AccountDetail acc) {
+        try {
+            String sql = "SELECT [Medical_Staff].[ID]\n"
+                    + "      ,[Level of education]\n"
+                    + "      ,[Hospital]\n"
+                    + "  FROM [Medical_Staff]\n"
+                    + "  INNER JOIN [Account_Details] ON [Medical_Staff].[ID] = [Account_Details].[ID]\n"
+                    + "  WHERE [Account_Details].[ID] = ?";
+            PreparedStatement stm = connection.prepareCall(sql);
+            stm.setString(1, acc.getAccount().getUserName());
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                MedicalStaff med = new MedicalStaff();
+                med.setAccount(acc);
+                med.setLevelOfEducation(rs.getNString("Level of education"));
+                med.setHospital(rs.getNString("Hospital"));
+                return med;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(AccountDetailDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+}
--- web/admin/profile.jsp
+++ web/admin/profile.jsp
@@ -1,3 +1,8 @@
+<%-- 
+    Document   : profile
+    Created on : Sep 16, 2022, 1:59:03 AM
+    Author     : Mountain
+--%>
 
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
@@ -22,7 +27,18 @@
                         ${r.role}
                     </option>
                 </c:forEach>
-            </select><br/>`
+            </select><br/>
+            <c:if test="${requestScope.account.account.role.id eq 2 || requestScope.account.account.role.id eq 3}">
+                Level of education: <input type="text" name="lvlofedu" value="${requestScope.medical.levelOfEducation}"/><br/>
+                Hospital: <input type="text" name="hospital" value="${requestScope.medical.hospital}"/><br/>
+            </c:if>
+            <c:if test="${requestScope.account.account.role.id eq 4}">
+                Room: ${requestScope.patient.room.name}<br/>
+                Area: ${patient.room.area.name}<br/>
+                Background disease: <c:if test="${requestScope.patient.backgroundDisease eq true}">Yes</c:if>
+                <c:if test="${requestScope.patient.backgroundDisease eq false}">No</c:if><br/>
+                Blood type: <input type="text" name="blood" value="${patient.bloodType}"/><br/>
+            </c:if>
             <input type="submit" value="Save">
         </form>
     </body>
